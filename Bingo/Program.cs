9726865d7f0ad7a01fe31013ea59b1cf318dc156
Henry Baldacci <56937203+hbyr99@users.noreply.github.com>
2021-01-29 11:44:13 -0500

    Initial commit
    A program to find relationships in a family tree.

---
+++ Bingo/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Bingo
+{
+    class Program
+    {
+        private static RelationshipGraph rg;
+
+        // Read RelationshipGraph whose filename is passed in as a parameter.
+        // Build a RelationshipGraph in RelationshipGraph rg
+        private static void ReadRelationshipGraph(string filename)
+        {
+            rg = new RelationshipGraph();                           // create a new RelationshipGraph object
+
+            string name = "";                                       // name of person currently being read
+            int numPeople = 0;
+            string[] values;
+            Console.Write("Reading file " + filename + "\n");
+            try
+            {
+                string input = System.IO.File.ReadAllText(filename);// read file
+                input = input.Replace("\r", ";");                   // get rid of nasty carriage returns 
+                input = input.Replace("\n", ";");                   // get rid of nasty new lines
+                string[] inputItems = Regex.Split(input, @";\s*");  // parse out the relationships (separated by ;)
+                foreach (string item in inputItems) 
+		{
+                    if (item.Length > 2)                            // don't bother with empty relationships
+                    {
+                        values = Regex.Split(item, @"\s*:\s*");     // parse out relationship:name
+                        if (values[0] == "name")                    // name:[personname] indicates start of new person
+                        {
+                            name = values[1];                       // remember name for future relationships
+                            rg.AddNode(name);                       // create the node
+                            numPeople++;
+                        }
+                        else
+                        {               
+                            rg.AddEdge(name, values[1], values[0]); // add relationship (name1, name2, relationship)
+
+                            // handle symmetric relationships -- add the other way
+                            if (values[0] == "hasSpouse" || values[0] == "hasFriend")
+                                rg.AddEdge(values[1], name, values[0]);
+
+                            // for parent relationships add child as well
+                            else if (values[0] == "hasParent")
+                                rg.AddEdge(values[1], name, "hasChild");
+                            else if (values[0] == "hasChild")
+                                rg.AddEdge(values[1], name, "hasParent");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write("Unable to read file {0}: {1}\n", filename, e.ToString());
+            }
+            Console.WriteLine(numPeople + " people read");
+        }
+
+        // Show the relationships a person is involved in
+        private static void ShowPerson(string name)
+        {
+            GraphNode n = rg.GetNode(name);
+            if (n != null)
+                Console.Write(n.ToString());
+            else
+                Console.WriteLine("{0} not found", name);
+        }
+
+        // Show a person's friends
+        private static void ShowFriends(string name)
+        {
+            GraphNode n = rg.GetNode(name);
+            if (n != null)
+            {
+                Console.Write("{0}'s friends: ",name);
+                List<GraphEdge> friendEdges = n.GetEdges("hasFriend");
+                foreach (GraphEdge e in friendEdges) {
+                    Console.Write("{0} ",e.To());
+                }
+                Console.WriteLine();
+            }
+            else
+                Console.WriteLine("{0} not found", name);     
+        }
+
+        // Show orphans
+        private static void ShowOrphans()
+        {
+            int _;
+
+            // Check all nodes in rg
+            foreach (GraphNode n in rg.nodes)
+            {
+                List<GraphEdge> nEdges = n.GetEdges();
+                int isOrphan = 0;
+
+
+            // Check all edges for hasParent label
+                foreach (GraphEdge i in nEdges)
+                {
+                    if (i.Label == "hasParent") { isOrphan++; break; }
+                }
+                if (isOrphan == 0 && !int.TryParse(n.Name, out _))
+                {
+                    Console.Write("{0} is an orphan.\n", n.Name);
+                }
+            }
+        }
+
+        // Show a person's friends
+        private static void ShowSiblings(string name)
+        {
+            GraphNode n = rg.GetNode(name);
+            List<string> siblings = new List<string>();
+
+            if (n != null)
+            {
+                Console.Write("{0}'s siblings: ", name);
+                List<GraphEdge> parentEdges = n.GetEdges("hasParent");
+                
+                foreach (GraphEdge e in parentEdges)
+                {
+                    string parentName = e.To();
+                    GraphNode parentNode = rg.GetNode(parentName);
+                    List<GraphEdge> childrenEdges = parentNode.GetEdges("hasChild");
+                    
+                    foreach (GraphEdge i in childrenEdges)
+                    {
+                        if (!siblings.Contains(i.To()) && i.To() != name) { siblings.Add(i.To()); }
+                    }
+                }
+
+                foreach (string sibName in siblings) { Console.Write("{0} ", sibName); }
+                Console.WriteLine();
+            }
+            else
+                Console.WriteLine("{0} not found", name);
+        }
+
+        // Show a person's descendants
+        private static void ShowDescendants(string name)
+        {
+            GraphNode n = rg.GetNode(name);
+            if (n == null)
+            {
+                Console.WriteLine("{0} not found", name);
+                return;
+            }
+
+            List<GraphEdge> childEdges = n.GetEdges("hasChild");
+            Dictionary<string, string> output = FindChildren(new Dictionary<string, string>(), childEdges, 0);
+            if (output == null)
+            {
+                Console.WriteLine("No descendants found");
+            }
+            else
+            {
+                foreach (KeyValuePair<string, string> desc in output)
+                {
+                    Console.WriteLine("{0} is a {1}", desc.Key, desc.Value);
+                }
+            }
+        }
+
+        // Recursive function to find descendants
+        private static Dictionary<string, string> FindChildren(Dictionary<string, string> dict, List<GraphEdge> lstEdges, int count)
+        {
+            if (!lstEdges.Any()) return null;
+
+            string suffix = "child";
+            if (count > 1) suffix = "grand" + suffix;
+            if (count > 2 )
+            {
+                int i = count;
+                while (i > 2 )
+                {
+                    suffix = "great " + suffix;
+                    i--;
+                }
+            }
+
+            foreach( GraphEdge edges in lstEdges)
+            {
+                dict.TryAdd(edges.To(), suffix);
+                GraphNode temp = rg.GetNode(edges.To());
+                if (temp != null)
+                {
+                    FindChildren(dict, temp.GetEdges("hasChild"), count++);
+                }
+            }
+            return dict;
+        }
+
+        // Bingo function
+        private static void Bingo(string name1, string name2)
+        {
+            List<GraphNode> path = rg.ShortestPath(name1, name2);
+            if (path != null)
+            {
+                for (int i = 1; i < path.Count; i++)
+                {
+                    foreach (GraphEdge e in path[i - 1].GetEdges())
+                    {
+                        if (rg.GetNode(e.To()) == path[i])
+                        {
+                            string e_String = "";
+                            if (e.Label == "hasParent") e_String = "parent";
+                            if (e.Label == "hasChild") e_String = "child";
+                            if (e.Label == "hasSpouse") e_String = "spouse";
+                            Console.WriteLine(path[i].Name + " is a " + e_String + " of " + path[i-1].Name);
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine(name1 + " has no relation to " + name2);
+        }
+
+
+        // accept, parse, and execute user commands
+        private static void CommandLoop()
+        {
+            string command = "";
+            string[] commandWords;
+            Console.Write("Welcome to Harry's Dutch Bingo Parlor!\n");
+
+            while (command != "exit")
+            {
+                Console.Write("\nEnter a command: ");
+                command = Console.ReadLine();
+                commandWords = Regex.Split(command, @"\s+");        // split input into array of words
+                command = commandWords[0];
+
+                if (command == "exit")
+                    ;                                               // do nothing
+
+                // read a relationship graph from a file
+                else if (command == "read" && commandWords.Length > 1)
+                    ReadRelationshipGraph(commandWords[1]);
+
+                // show information for one person
+                else if (command == "show" && commandWords.Length > 1)
+                    ShowPerson(commandWords[1]);
+
+                else if (command == "friends" && commandWords.Length > 1)
+                    ShowFriends(commandWords[1]);
+
+                else if (command == "orphans")
+                    ShowOrphans();
+
+                else if (command == "siblings" && commandWords.Length > 1)
+                    ShowSiblings(commandWords[1]);
+
+                else if (command == "descendants" && commandWords.Length > 1)
+                    ShowDescendants(commandWords[1]);
+
+                else if (command == "bingo" && commandWords.Length > 2)
+                    Bingo(commandWords[1], commandWords[2]);
+
+                // dump command prints out the graph
+                else if (command == "dump")
+                    rg.Dump();
+
+                // illegal command
+                else
+                    Console.Write("\nLegal commands: read [filename], dump, show [personname],\n  friends [personname], orphans, siblings [personname],\n descendants [personname] exit\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLoop();
+        }
+    }
+}
---
+++ Bingo/RelationshipGraph.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bingo
+{
+    /// <summary>
+    /// Represents a directed labeled graph with a string name at each node
+    /// and a string Label for each edge.
+    /// </summary>
+    class RelationshipGraph
+    {
+        /*
+         *  This data structure contains a list of nodes (each of which has
+         *  an adjacency list) and a dictionary (hash table) for efficiently 
+         *  finding nodes by name
+         */
+        public List<GraphNode> nodes { get; private set; }
+        private Dictionary<String, GraphNode> nodeDict;
+
+        // constructor builds empty relationship graph
+        public RelationshipGraph()
+        {
+            nodes = new List<GraphNode>();
+            nodeDict = new Dictionary<String,GraphNode>();
+        }
+
+        // AddNode creates and adds a new node if there isn't already one by that name
+        public void AddNode(string name)
+        {
+            if (!nodeDict.ContainsKey(name))
+            {
+                GraphNode n = new GraphNode(name);
+                nodes.Add(n);
+                nodeDict.Add(name, n);
+            }
+        }
+
+        // AddEdge adds the edge, creating endpoint nodes if necessary.
+        // Edge is added to adjacency list of from edges.
+        public void AddEdge(string name1, string name2, string relationship) 
+        {
+            AddNode(name1);                     // create the node if it doesn't already exist
+            GraphNode n1 = nodeDict[name1];     // now fetch a reference to the node
+            AddNode(name2);
+            GraphNode n2 = nodeDict[name2];
+            GraphEdge e = new GraphEdge(n1, n2, relationship);
+            n1.AddIncidentEdge(e);
+        }
+
+        // Get a node by name using dictionary
+        public GraphNode GetNode(string name)
+        {
+            if (nodeDict.ContainsKey(name))
+                return nodeDict[name];
+            else
+                return null;
+        }
+
+        // Return a text representation of graph
+        public void Dump()
+        {
+            foreach (GraphNode n in nodes)
+            {
+                Console.Write(n.ToString());
+            }
+        }
+
+		public List<GraphNode> ShortestPath(String F, String T)
+		{
+			// BFS search for shortest path
+			List<GraphNode> path = new List<GraphNode>();
+			List<List<GraphNode>> levels = new List<List<GraphNode>>();
+			List<GraphNode> level = new List<GraphNode>();
+			Dictionary<String, Boolean> seen = new Dictionary<String, Boolean>();
+			int levelCount = 0;
+			GraphNode From = GetNode(F);
+			GraphNode To = GetNode(T);
+
+			if (From == null || To == null || From == To)
+				return null;
+
+			level.Add(From);
+			levels.Add(level);
+			while (true)
+			{
+				level = new List<GraphNode>();
+				foreach (GraphNode n in levels[levels.Count - 1])
+				{
+					foreach (GraphEdge e in n.GetEdges())
+					{
+						if (e.To() == T) // End node -- stuck
+							goto EndBuild;
+						if (!seen.ContainsKey(e.To()))// Avoid equal keys
+						{
+							level.Add(GetNode(e.To()));
+							seen.Add(e.To(), true);
+						}
+					}
+				}
+				levels.Add(level);
+			}
+		EndBuild:
+			// Reverse path
+			path.Add(To);
+			levelCount = levels.Count - 1;
+			try
+			{
+				while (path[path.Count - 1] != From && levelCount >= 0)
+				{ 
+					foreach (GraphEdge e in path[path.Count - 1].GetEdges())
+					{
+						if (levels[levelCount].Contains(GetNode(e.To())))
+						{
+							path.Add(GetNode(e.To()));
+							goto NextInWhile;
+						}
+					}
+
+					foreach (GraphNode n in levels[levelCount])
+					{
+						foreach (GraphEdge e in n.GetEdges())
+						{
+							if (GetNode(e.To()) == path[path.Count - 1])
+							{
+								path.Add(n);
+								goto NextInWhile;
+							}
+						}
+					}
+				NextInWhile:
+					levelCount--;
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			path.Reverse();
+			if (path.Count == 1)
+				path = null;
+			return path;
+		}
+	}
+}
